a5b5c3c4dcfe07ebabf3c68a188f764eb2cf6ba3
Daniel Gonzalez <danielmtzlpz95@gmail.com>
2023-07-23 20:59:08 -0600

    Actualizacion semi finalizado

--- JS/site.js
+++ JS/site.js
@@ -10,13 +10,13 @@
   
   // Se selecciona todos los elementos que se vana a animar
   let to_animated_elements = document.querySelectorAll(
-    ".cuerpo .vista .presentacion .cuerpo_panel p, .cuerpo .vista .presentacion .pie_de_pagina .area_navegacion .lista_componentes .opcion, .cuerpo .vista .informacion .cabecera_pagina .area_navegacion .lista_componentes .opcion, .cuerpo .vista .informacion .cuerpo_panel .card .titulo_nombre, .cuerpo .vista .informacion .cuerpo_panel .card .message span"
+    ".cuerpo .vista .presentacion .cuerpo_panel p, .cuerpo .vista .presentacion .pie_de_pagina .area_navegacion .lista_componentes .opcion, .cuerpo .vista .informacion .cabecera_pagina .area_navegacion .lista_componentes .opcion, .cuerpo .vista .informacion .cuerpo_panel .card .titulo_nombre, .cuerpo .vista .informacion .cuerpo_panel .card .message span, .cuerpo .vista .proyectos .cuerpo_panel .card .title, .cuerpo .vista .proyectos .cuerpo_panel .card .descripcion span"
   );
 
   // Se inicia una duncion que permite dormir el proceso de ejecucion
   const sleep = (ms) => new Promise((r) => setTimeout(r, ms));
 
-  // console.log(to_animated_elements);
+  console.log(to_animated_elements);
 
   // Una funcion que evita que se haga de manera paralela la asignacion de animaciones de golpe y animaar uno por uno los elementos html
   const recursividad_antiparalelismo = async (i) => {
